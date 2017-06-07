7cd09b3ca2226781080b59a4f8152ddc7c69f5c1
zww <zww@maizjr.com>
2017-06-07 09:18:43 +0800

    Snackbar

---
+++ app/src/main/java/com/vv/zvv/Utils/ToastUtil_01.java
@@ -0,0 +1,201 @@
+package com.vv.zvv.Utils;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by zvv on 2017/6/5 14:31.
+ */
+public class ToastUtil_01 {
+    /**
+     * Created by 赵晨璞 on 2016/8/11.
+     */
+
+    private Toast toast;
+    private LinearLayout toastView;
+
+    /**
+     * 修改原布局的Toast
+     */
+    public ToastUtil_01() {
+
+    }
+
+    /**
+     * 完全自定义布局Toast
+     *
+     * @param context
+     * @param view
+     */
+    public ToastUtil_01(Context context, View view, int duration) {
+        toast = new Toast(context);
+        toast.setView(view);
+        toast.setDuration(duration);
+    }
+
+    /**
+     * 向Toast中添加自定义view
+     *
+     * @param view
+     * @param postion
+     * @return
+     */
+    public ToastUtil_01 addView(View view, int postion) {
+        toastView = (LinearLayout) toast.getView();
+        toastView.addView(view, postion);
+
+        return this;
+    }
+
+    /**
+     * 设置Toast字体及背景颜色
+     *
+     * @param messageColor
+     * @param backgroundColor
+     * @return
+     */
+    public ToastUtil_01 setToastColor(int messageColor, int backgroundColor) {
+        View view = toast.getView();
+        if (view != null) {
+            TextView message = ((TextView) view.findViewById(android.R.id.message));
+            message.setBackgroundColor(backgroundColor);
+            message.setTextColor(messageColor);
+        }
+        return this;
+    }
+
+    /**
+     * 设置Toast字体及背景
+     *
+     * @param messageColor
+     * @param background
+     * @return
+     */
+    public ToastUtil_01 setToastBackground(int messageColor, int background) {
+        View view = toast.getView();
+        if (view != null) {
+            TextView message = ((TextView) view.findViewById(android.R.id.message));
+            message.setBackgroundResource(background);
+            message.setTextColor(messageColor);
+        }
+        return this;
+    }
+
+    /**
+     * 短时间显示Toast
+     */
+    public ToastUtil_01 Short(Context context, CharSequence message) {
+        if (toast == null || (toastView != null && toastView.getChildCount() > 1)) {
+            toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+            toastView = null;
+        } else {
+            toast.setText(message);
+            toast.setDuration(Toast.LENGTH_SHORT);
+        }
+        return this;
+    }
+
+    /**
+     * 短时间显示Toast
+     */
+    public ToastUtil_01 Short(Context context, int message) {
+        if (toast == null || (toastView != null && toastView.getChildCount() > 1)) {
+            toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+            toastView = null;
+        } else {
+            toast.setText(message);
+            toast.setDuration(Toast.LENGTH_SHORT);
+        }
+        return this;
+    }
+
+    /**
+     * 长时间显示Toast
+     */
+    public ToastUtil_01 Long(Context context, CharSequence message) {
+        if (toast == null || (toastView != null && toastView.getChildCount() > 1)) {
+            toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+            toastView = null;
+        } else {
+            toast.setText(message);
+            toast.setDuration(Toast.LENGTH_LONG);
+        }
+        return this;
+    }
+
+    /**
+     * 长时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public ToastUtil_01 Long(Context context, int message) {
+        if (toast == null || (toastView != null && toastView.getChildCount() > 1)) {
+            toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+            toastView = null;
+        } else {
+            toast.setText(message);
+            toast.setDuration(Toast.LENGTH_LONG);
+        }
+        return this;
+    }
+
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public ToastUtil_01 Indefinite(Context context, CharSequence message, int duration) {
+        if (toast == null || (toastView != null && toastView.getChildCount() > 1)) {
+            toast = Toast.makeText(context, message, duration);
+            toastView = null;
+        } else {
+            toast.setText(message);
+            toast.setDuration(duration);
+        }
+        return this;
+    }
+
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public ToastUtil_01 Indefinite(Context context, int message, int duration) {
+        if (toast == null || (toastView != null && toastView.getChildCount() > 1)) {
+            toast = Toast.makeText(context, message, duration);
+            toastView = null;
+        } else {
+            toast.setText(message);
+            toast.setDuration(duration);
+        }
+        return this;
+    }
+
+    /**
+     * 显示Toast
+     *
+     * @return
+     */
+    public ToastUtil_01 show() {
+        toast.show();
+
+        return this;
+    }
+
+    /**
+     * 获取Toast
+     *
+     * @return
+     */
+    public Toast getToast() {
+        return toast;
+    }
+}
