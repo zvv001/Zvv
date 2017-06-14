efe490bee051723932b3d7e3a78466fca952031c
zww <zww@maizjr.com>
2017-06-14 13:55:17 +0800

    Snackbar

---
+++ app/src/main/java/com/vv/zvv/JavaBean/Classes.java
@@ -0,0 +1,27 @@
+package com.vv.zvv.JavaBean;
+
+import java.util.List;
+
+/**
+ * Created by zvv on 2017/6/7 09:39.
+ */
+public class Classes {
+    String className;
+    List<String> studentList;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public List<String> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<String> studentList) {
+        this.studentList = studentList;
+    }
+}
