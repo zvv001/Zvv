45360f6b4b2739335cbcbbb557d6142a7b502ea7
zww <zww@maizjr.com>
2017-08-03 09:27:27 +0800

    RegionSelectActivity: 地址选择器 指纹解锁

--- app/src/main/java/com/vv/zvv/activity/FingerActivity.java
+++ app/src/main/java/com/vv/zvv/activity/FingerActivity.java
@@ -32,6 +32,7 @@ public class FingerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finger);
+
         init();
     }
 
