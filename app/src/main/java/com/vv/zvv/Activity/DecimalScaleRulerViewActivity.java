bd119634a399f664de1f647fcd8c8e291da39446
zww <zww@maizjr.com>
2017-11-30 11:32:42 +0800

    2017-11-30 DecimalScaleRulerView 刻度游标尺

--- app/src/main/java/com/vv/zvv/Activity/DecimalScaleRulerViewActivity.java
+++ app/src/main/java/com/vv/zvv/Activity/DecimalScaleRulerViewActivity.java
@@ -1,7 +1,10 @@
 package com.vv.zvv.Activity;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -9,6 +12,7 @@ import com.vv.zvv.R;
 import com.vv.zvv.Utils.DrawUtil;
 import com.vv.zvv.Views.DecimalScaleRulerView;
 
+import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
@@ -40,6 +44,11 @@ public class DecimalScaleRulerViewActivity extends AppCompatActivity {
     @ViewInject(R.id.tv_progressPercent)
     private TextView tv_progressPercent;
 
+    @ViewInject(R.id.tv_showNumber)
+    private TextView tv_showNumber;
+
+
+    ValueAnimator valueAnimator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +57,36 @@ public class DecimalScaleRulerViewActivity extends AppCompatActivity {
         x.view().inject(this);
 
         init();
+        initAnimation();
+    }
+
+    @Event(value = {R.id.tv_showNumber}, type = View.OnClickListener.class)
+    private void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_showNumber:
+                valueAnimator.start();
+                break;
+
+        }
+    }
+
+    private void initAnimation() {
+        valueAnimator = ValueAnimator.ofFloat(0, 1000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float animatedValue = (float) valueAnimator.getAnimatedValue();
+
+                DecimalFormat decimalFormat = new DecimalFormat("#.00");
+                String formatStr = decimalFormat.format(animatedValue);
+                final float show = Float.parseFloat(formatStr);
+
+//                tv_showNumber.setText("$" + animatedValue);
+                tv_showNumber.setText("$" + show);
+            }
+        });
+        valueAnimator.setDuration(6000);
+
     }
 
     private void init() {
@@ -66,7 +105,7 @@ public class DecimalScaleRulerViewActivity extends AppCompatActivity {
             @Override
             public void onValueChange(float value) {
                 DecimalFormat df = new DecimalFormat("##0");
-                String showValue =  df.format(value*10000)+"元";
+                String showValue = df.format(value * 10000) + "元";
                 mWeightValueTwo.setText(showValue);
 
 
@@ -80,11 +119,26 @@ public class DecimalScaleRulerViewActivity extends AppCompatActivity {
                 tv_progressPercent.setText(showPercent);//展示百分比
             }
         });
+
+        Log.d("vv", "init: \n"
+                + "getCeil(10.12 000000000): " + getCeil(10.120000000001) + "\n"
+                + "getCeil(10.12 431): " + getCeil(10.12431) + "\n"
+                + "getCeil(10.12 481): " + getCeil(10.12481) + "\n"
+                + "getCeil(10.12 601): " + getCeil(10.12601) + "\n"
+                + "getCeil(10.12 681): " + getCeil(10.12681) + "\n"
+
+        );
+
     }
 
-    private String getCeil(double d) {
-        DecimalFormat df = new DecimalFormat("##0.0");
+    /**
+     * @向上取整
+     */
+    public static String getCeil(double d) {
+        // d=2.523214324;
+        DecimalFormat df = new DecimalFormat("###,##0.00");
         df.setRoundingMode(RoundingMode.UP);
         return df.format(d);
     }
+
 }
